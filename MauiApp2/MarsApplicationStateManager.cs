eba5bbf184d86d897fbf84caf52414f1120ed253
Pierce Kennedy <Pierce.Kennedy@mshp.dps.mo.gov>
2024-06-07 14:09:27 -0500

    Adding files

---
+++ MauiApp2/MarsApplicationStateManager.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp2;
+
+public class MarsApplicationStateManager : IMarsApplicationStateManager
+{
+    public MarsApplicationStateManager(ReportStateContainer reportState)
+    { 
+        _reportState = reportState;
+    }
+    private ReportStateContainer _reportState;
+    
+    public ReportStateContainer ReportState
+    {
+        get
+        {
+            return _reportState ?? (_reportState = new ReportStateContainer());
+        }
+        set
+        {
+            _reportState = value;
+        }
+    }
+}
---
+++ MauiApp2/ReportStateContainer.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp2;
+
+public class ReportStateContainer
+{
+    public Blazored.FluentValidation.FluentValidationValidator FluentValidationValidator { get; set; }
+    public bool isTouched = false;
+    public void OnTouched(bool b)
+    {
+        if (b)
+            isTouched = true;
+    }
+}
